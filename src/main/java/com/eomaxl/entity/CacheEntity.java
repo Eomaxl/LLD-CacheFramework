eedeedf3f3ed0b4ce6b6f91db0f11a07a7ed99dc
Eomaxl <souravsnigdh@gmail.com>
2024-08-26 01:34:22 +0530

    Updated the readme file

--- src/main/java/com/eomaxl/entity/CacheEntity.java
+++ src/main/java/com/eomaxl/entity/CacheEntity.java
@@ -1,12 +1,10 @@
 package com.eomaxl.entity;
-
 /**
  * A class representing a single cache entity.
  *
  * @param <K> the type of keys maintained by the cache
  * @param <V> the type of mapped values
  */
-
 public class CacheEntity<K, V> {
     private final K key;
     private final V value;
